fb9bf4548b156b7ed934fd366b5dbac6355c92c1
dickyandeyan <dickyundead@gmail.com>
2022-02-22 23:17:37 +0700

    create collapsible navbar and content

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,15 @@
-$(document).ready(function(){       
-   var scroll_pos = 0;
-   $(document).scroll(function() { 
-       scroll_pos = $(this).scrollTop();
-       if(scroll_pos > 210) {
-           $('#navbar-fix').addClass('gradient');
-       }
-   });
-});
+$(document).ready(function () {
+	var scroll_pos = 0
+	$(document).scroll(function () {
+		scroll_pos = $(this).scrollTop()
+		if (scroll_pos > 210) {
+			$('#navbar-fix').addClass('gradient')
+		}
+	})
+})
+const openNav = () => {
+	$('#navbarContent').toggleClass('h-100')
+	$('#navbar-icon').toggleClass('fa-solid fa-xmark')
+	$('#navbar-icon').toggleClass('fa-solid fa-bars')
+   $('#btn-login, #menu-list').toggleClass('d-none')
+}
